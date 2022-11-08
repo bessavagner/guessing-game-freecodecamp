48ede59c1d974a28fd963cc61e3ffcd1748cc73a
bessavagner <bessavagner@gmail.com>
2022-11-08 04:00:45 +0000

    fix: deleted exceeded lines

--- number_guess.sh
+++ number_guess.sh
@@ -3,7 +3,6 @@
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 MAIN() {
-	echo -e "\n======== Number Guessing Game ========\n"
 	echo -e "Enter your username:"
 	read USERNAME
 
@@ -24,9 +23,9 @@ MAIN() {
 		# games played
 		GAMES_PLAYED=$($PSQL "SELECT count(user_id) FROM games WHERE user_id=$USER_ID")
 		# best game
-		BEST_SCORE=$($PSQL "SELECT min(guesses) FROM games WHERE user_id=$USER_ID")
+		BEST_GAME=$($PSQL "SELECT min(guesses) FROM games WHERE user_id=$USER_ID")
 
-		echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_SCORE guesses."
+		echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 	fi
 	GAME
 }
@@ -34,7 +33,7 @@ MAIN() {
 GAME() {
 	SECRET_NUMBER=$(($RANDOM % 1000 + 1))
 	GUESS=0
-	GUESSES=0
+	TRIES=0
 
 	echo -e "\nGuess the secret number between 1 and 1000:"
 
@@ -48,22 +47,21 @@ GAME() {
 		# if correct
 		elif [[ $SECRET_NUMBER = $GUESS ]]
 		then
-			GUESSES=$(($GUESSES + 1))
-			echo -e "\nYou guessed it in $GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+			TRIES=$(($TRIES + 1))
+			echo -e "\nYou guessed it in $TRIES tries. The secret number was $SECRET_NUMBER. Nice job!"
 			#insert into db
-			INSERT_GAME_RESULT=$($PSQL "insert into games(user_id, guesses) values($USER_ID, $GUESSES)")
+			INSERT_GAME_RESULT=$($PSQL "insert into games(user_id, guesses) values($USER_ID, $TRIES)")
 		# if greater
 		elif [[ $SECRET_NUMBER -gt $GUESS ]]
 		then
-			GUESSES=$(($GUESSES + 1))
+			TRIES=$(($TRIES + 1))
 			echo -e "\nIt's higher than that, guess again:"
 		# if lower
 		else
-			GUESSES=$(($GUESSES + 1))
+			TRIES=$(($TRIES + 1))
       		echo -e "\nIt's lower than that, guess again:"
 		fi
 	done
-	echo -e "\nThanks for playing!"
 }
 
 MAIN
