88d79a3c3666f6c947318f5fd8d52e3b65e1caab
bessavagner <bessavagner@gmail.com>
2022-11-08 00:12:01 -0300

    feat: game function

--- number_guess.sh
+++ number_guess.sh
@@ -26,8 +26,43 @@ MAIN() {
 		# best game
 		BEST_SCORE=$($PSQL "SELECT min(guesses) FROM games WHERE user_id=$USER_ID")
 
-		echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GUESS guesses."
+		echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_SCORE guesses."
 	fi
+	GAME
+}
+
+GAME() {
+	SECRET_NUMBER=$(($RANDOM % 1000 + 1))
+	GUESS=0
+	GUESSES=0
+
+	echo -e "\nGuess the secret number between 1 and 1000:"
+
+	while [[ $GUESS != $SECRET_NUMBER ]]
+	do
+		read GUESS
+		# if not integer
+		if [[ ! $GUESS =~ ^[0-9]+$ ]]
+		then
+			echo -e "\nThat is not an integer, guess again:"
+		# if correct
+		elif [[ $SECRET_NUMBER = $GUESS ]]
+		then
+			GUESSES=$(($GUESSES + 1))
+			echo -e "\nYou guessed it in $GUESSES guesses. The secret number was $SECRET_NUMBER. Nice job!"
+			#insert into db
+			INSERT_GAME_RESULT=$($PSQL "insert into games(user_id, guesses) values($USER_ID, $GUESSES)")
+		# if greater
+		elif [[ $SECRET_NUMBER -gt $GUESS ]]
+		then
+			GUESSES=$(($GUESSES + 1))
+			echo -e "\nIt's higher than that, guess again:"
+		# if lower
+		else
+			GUESSES=$(($GUESSES + 1))
+      		echo -e "\nIt's lower than that, guess again:"
+		fi
+	done
 }
 
 MAIN
